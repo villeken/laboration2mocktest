964fd2dac78e1fd1906bd5bd0773aba8a4c421db
Vilhelm Kennedal <vilhelm.kennedal@iths.se>
2025-02-02 11:11:56 +0100

    add shoppingCart Class and TestClass

---
+++ src/test/java/com/example/ShoppingCartTest.java
@@ -0,0 +1,14 @@
+package com.example;
+
+import com.example.payment.ShoppingCart;
+import org.junit.jupiter.api.BeforeEach;
+
+public class ShoppingCartTest {
+
+    private ShoppingCart shoppingCart;
+
+    @BeforeEach
+    void setup() {
+        shoppingCart = new ShoppingCart();
+    }
+}
