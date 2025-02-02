d9ee92de1e0ce8bdfee4ea8ab269bfb8481a5ccb
Vilhelm Kennedal <vilhelm.kennedal@iths.se>
2025-02-02 11:47:46 +0100

    Add code in ShoppingCart to pass test

--- src/main/java/com/example/exercise2/ShoppingCart.java
+++ src/main/java/com/example/exercise2/ShoppingCart.java
@@ -1,6 +1,20 @@
 package com.example.exercise2;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class ShoppingCart {
 
+    private final Map<Product, Integer> products = new HashMap<>();
+    private int totalPrice = 0;
 
+    public void addProduct(Product product, int quantity) {
+        products.put(product, products.getOrDefault(product, 0) + quantity);
+        totalPrice += (int) (product.getPrice() * quantity);
+    }
+
+    public int getTotalPrice() {
+        return totalPrice;
+    }
 }
+
